fa9c5ecaa2d67fa3801a37a04e12fefa54bcb1e6
charanvinay <71263421+charanvinay@users.noreply.github.com>
2022-01-31 19:00:57 +0530

    created fake database for projects

--- ui/src/Components/Dashboard/Projects/Projects.js
+++ ui/src/Components/Dashboard/Projects/Projects.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from ".././Dashboard.module.css";
 import projectIcon from "../../../../src/Assets/project_icon.png";
 import { RiCloseLine } from "react-icons/ri";
@@ -7,99 +7,24 @@ import Tooltip from "react-bootstrap/Tooltip";
 
 function Projects() {
   const [selectedProject, setselectedProject] = useState({});
+  const [projects, setProjects] = useState(null);
 
   let projectIndex;
-  let projects = [];
-  let tools = [
-    {
-      name: "Bootstrap",
-      url: "https://getbootstrap.com/",
-      desc: "desc1",
-    },
-    {
-      name: "Google",
-      url: "https://www.google.com/",
-      desc: "desc2",
-    },
-    {
-      name: "Youtube",
-      url: "https://www.youtube.com/",
-      desc: "desc3",
-    },
-    {
-      name: "Stack overflow",
-      url: "https://stackoverflow.com/",
-      desc: "desc4",
-    },
-    {
-      name: "Github",
-      url: "https://github.com/",
-      desc: "desc5",
-    },
-    {
-      name: "Bootstrap",
-      url: "https://getbootstrap.com/",
-      desc: "desc6",
-    },
-    {
-      name: "Google",
-      url: "https://www.google.com/",
-      desc: "desc7",
-    },
-    {
-      name: "Youtube",
-      url: "https://www.youtube.com/",
-      desc: "desc8",
-    },
-    {
-      name: "Stack overflow",
-      url: "https://stackoverflow.com/",
-      desc: "desc9",
-    },
-    {
-      name: "Github",
-      url: "https://github.com/",
-      desc: "desc10",
-    },
-    {
-      name: "Bootstrap",
-      url: "https://getbootstrap.com/",
-      desc: "desc11",
-    },
-    {
-      name: "Google",
-      url: "https://www.google.com/",
-      desc: "desc12",
-    },
-    {
-      name: "Youtube",
-      url: "https://www.youtube.com/",
-      desc: "desc13",
-    },
-    {
-      name: "Stack overflow",
-      url: "https://stackoverflow.com/",
-      desc: "desc14",
-    },
-    {
-      name: "Github",
-      url: "https://github.com/",
-      desc: "desc15",
-    },
-  ];
 
-  for (let index = 1; index <= 15; index++) {
-    projects.push({
-      id: "p" + index,
-      name: "Project " + index,
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
-      tools: tools,
-    });
-  }
+  const fetchProjects = async () => {
+    const response = await fetch("http://localhost:5000/projects");
+    const projects = await response.json();
+    setProjects(projects);
+    console.log(projects);
+  };
+
+  useEffect(() => {
+    fetchProjects();
+  }, []);
 
   const openProject = (id) => {
     projectIndex = projects.findIndex((p) => p.id === id);
-    console.log(projects[projectIndex]);
+    // console.log(projects[projectIndex]);
     setselectedProject(projects[projectIndex]);
   };
 
@@ -112,21 +37,25 @@ function Projects() {
       <div className="col">
         <div className="container-fluid">
           <div className="row p-4">
-            {projects.map((project) => (
-              <div className="col-xl-2 col-md-4 col-sm-6 mb-4" key={project.id}>
+            {projects &&
+              projects.map((project) => (
                 <div
-                  className={styles.project_card}
-                  onClick={() => openProject(project.id)}
+                  className="col-xl-2 col-md-4 col-sm-6 mb-4"
+                  key={project.id}
                 >
-                  <img
-                    src={projectIcon}
-                    alt="Project"
-                    className={styles.img_fluid}
-                  />
-                  <p>{project.name}</p>
+                  <div
+                    className={styles.project_card}
+                    onClick={() => openProject(project.id)}
+                  >
+                    <img
+                      src={projectIcon}
+                      alt="Project"
+                      className={styles.img_fluid}
+                    />
+                    <p>{project.title}</p>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
           </div>
         </div>
       </div>
@@ -142,8 +71,8 @@ function Projects() {
                   onClick={() => closeProject()}
                 />
               </div>
-              <h4>{selectedProject.name}</h4>
-              <p>{selectedProject.desc}</p>
+              <h4>{selectedProject.title}</h4>
+              <p>{selectedProject.description}</p>
               <h4>Tools</h4>
               {selectedProject.tools?.map((tool, index) => (
                 <div
